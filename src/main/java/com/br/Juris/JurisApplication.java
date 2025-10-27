8f4b73d1d1e14c13f3baf0af3f0b089ae4bc9adc
Lucas <lucasrczz.01@gmail.com>
2025-10-27 10:56:38 -0300

    Inicialização do backend do Juris

---
+++ src/main/java/com/br/Juris/JurisApplication.java
@@ -0,0 +1,13 @@
+package com.br.Juris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JurisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JurisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/Juris/JurisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.Juris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JurisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
