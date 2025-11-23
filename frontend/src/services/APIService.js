2e885450331a6fc0beebdbfceedfaca09cd9861b
Fernanda Genú <nandafmgenu@gmail.com>
2025-11-23 15:01:41 -0300

    feat(frontend): enviando POST no endpoint processos

--- frontend/src/services/APIService.js
+++ frontend/src/services/APIService.js
@@ -2,12 +2,22 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
+const api = axios.create({
+    baseURL: API_URL,
+    headers: { 'Content-Type': 'multipart/form-data' }
+});
+
 export async function getFunction() {
-    const response = await axios.get(`${API_URL}/processos`);
+    const response = await api.get(`/processos`);
     return response.data;
 }
 
-// export async function postFunction() {
-//     const response = await axios.post(`${API_URL}/processos`, {status: 'Em andamento', numero: 99});
-//     return response.data;
-// }
+export async function postFunction() {
+    const response = await api.post(`/processos`, {
+        numero: "12345/2024",
+        status: "EM_ANDAMENTO",
+        observacoes: "Teste via frontend",
+        estado: "SP"
+    });
+    return response.data;
+}
